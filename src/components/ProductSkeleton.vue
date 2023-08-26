414e1fefccd98f868ee4ef2742dd901de8e68b03
mazufik <taufikprincecode10@gmail.com>
2023-08-26 22:37:07 +0900

    VIX Task Frontend Project

---
+++ src/components/ProductSkeleton.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="product">
+    <div class="product_img">
+      <AnimatedPlaceholder width="250px" height="250px" />
+    </div>
+    <div class="product_content">
+      <AnimatedPlaceholder />
+      <AnimatedPlaceholder width="70%" mt="8px" />
+      <div class="product_header-flex" :style="{ marginTop: '30px' }">
+        <AnimatedPlaceholder width="100px" />
+        <AnimatedPlaceholder width="25px" />
+      </div>
+      <div
+        class="product_description"
+        :style="{ minHeight: '100px', marginTop: '20px' }"
+      >
+        <AnimatedPlaceholder />
+        <AnimatedPlaceholder mt="8px" />
+        <AnimatedPlaceholder width="80%" mt="8px" />
+        <AnimatedPlaceholder width="40%" mt="8px" />
+      </div>
+      <AnimatedPlaceholder width="100px" height="20px" mt="20px" mb="30px" />
+      <div class="cta">
+        <AnimatedPlaceholder width="150px" height="40px" :style="{ flex: 1 }" />
+        <AnimatedPlaceholder width="150px" height="40px" :style="{ flex: 1 }" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AnimatedPlaceholder from "./AnimatedPlaceholder.vue";
+
+export default {
+  name: "ProductSkeleton",
+  components: {
+    AnimatedPlaceholder,
+  },
+};
+</script>
